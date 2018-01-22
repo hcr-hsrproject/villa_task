8462d5da413717ea28c371218b7780718e70ba2f
= <ssteveminq@gmail.com>
2018-01-22 11:17:49 -0600

    setting up experiment

--- src/villa_task/helpmecarry/following_srv_client.py
+++ src/villa_task/helpmecarry/following_srv_client.py
@@ -1,19 +1,27 @@
+#!/usr/bin/python
+
 import rospy
 from human_filter.srv import set_target_to_follow
-
+from std_msgs.msg import Int8
+from std_msgs.msg import Bool
 
 class HumanFollowingManager(object):
     def __init__(self):
         self.person_following_client = rospy.ServiceProxy("following_human", set_target_to_follow)
         self.person_following_client.wait_for_service(timeout=5)
         rospy.Subscriber('following_act_cmd', Bool, self.activation_callback)
+        rospy.Subscriber('cmd_trackhuman', Int8, self.cmd_int_callback)
         # self.follow_srv_cmd = std_msgs.msg.Bool()
     def activation_callback(self,msg):
-    	print("start following")
-    	self.start_following();
+        print("start following")
+        self.start_following();
+
+    def cmd_int_callback(self,msg):
+        print "cmd callback"
+        self.person_following_client(True)
 
     def listener(self):
-    	rospy.spin()
+        rospy.spin()
 
     def start_following(self):
         self.person_following_client(True)
@@ -25,4 +33,4 @@ if __name__ == '__main__':
     following_manager = HumanFollowingManager()
     print("Object created")
     #handover_manager.execute_handover()
-    handover_manager.listener()
+    following_manager.listener()
